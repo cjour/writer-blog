e97af602c2b5fc3a7b06a7e3766fe60a22a3ba1b
Clément J <61873476+cjour@users.noreply.github.com>
2020-04-14 18:39:36 +0200

    Delete errorView.php

--- Project 4 -DWJ/view/view_users/errorView.php
+++
@@ -1,7 +0,0 @@
-<?php $title = "Page d'erreur" ?>
-<?php ob_start();?>
-        <h1>Oups une erreur s'est produite.</h1>
-        <p><?= $errorMessage ?></p>
-<?php $content = ob_get_clean();
-require 'template.php';
-?>
